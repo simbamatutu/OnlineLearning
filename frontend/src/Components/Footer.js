c4f0e602c4823e170b9427d3856b9efc29bfc2e3
simbamatutu <simbamatutu@gmail.com>
2021-04-16 02:48:23 +0800

    1604 update model

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -10,7 +10,7 @@ export class Footer extends Component {
           backgroundColor: '#005bac',
           marginTop: '2rem',
           width: '100vw',
-          padding: '2rem 0 1rem 0',
+          padding: '2rem 0 1rem 0rem',
         }}
       >
         <Container className='pt-3'>
